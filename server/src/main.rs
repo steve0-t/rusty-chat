1be39700f5d101294c78d402e6d05bb361b9579f
steve0-t <stefan.simai@mailbox.org>
2026-06-22 22:35:41 +0200

    separete logic into separate functions

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,24 @@
-use std::{error::Error, fs::File, io::Read, net::TcpListener, rc::Rc, thread};
+use std::{
+    error::Error,
+    fs::File,
+    io::Read,
+    net::{TcpListener, TcpStream},
+    ops::Sub,
+    rc::Rc,
+    thread,
+};
 
 use websocket::{
     Message,
-    native_tls::{Identity, TlsAcceptor},
-    server::WsServer,
-    sync::Server,
+    native_tls::{Identity, TlsAcceptor, TlsStream},
+    server::{WsServer, upgrade::WsUpgrade},
+    sync::{Server, server::upgrade::Buffer},
 };
+
+enum CommandType {
+    CreateChannel = 2,
+    Quit = 1,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut cert_file = match File::open("cert.pem") {
@@ -61,31 +74,59 @@ fn deploy_server(server: &mut WsServer<TlsAcceptor, TcpListener>) {
         match conn {
             Ok(stream) => {
                 thread::spawn(move || {
-                    let mut client = stream.accept().unwrap();
-                    let message = Message::text("Server sent: Hello, client!");
-                    let _ = client.send_message(&message);
-
-                    loop {
-                        match client.recv_message() {
-                            Ok(msg) => {
-                                if msg.is_data() {
-                                    println!("Client sent: {msg:?}");
-                                }
-                            }
-
-                            Err(e) => {
-                                if !e.to_string().contains("NoDataAvailable") {
-                                    return;
-                                }
-                                println!("Received invalid message: {e:?}");
-                            }
-                        }
-                    }
+                    handle_client(stream);
                 });
             }
 
             Err(e) => {
                 eprintln!("Could not connect client: {e:?}");
+            }
+        }
+    }
+}
+
+fn handle_client(stream: WsUpgrade<TlsStream<TcpStream>, Option<Buffer>>) {
+    let mut client = stream.accept().unwrap();
+    let message = Message::text("Server sent: Hello, client!");
+    let _ = client.send_message(&message);
+
+    loop {
+        match client.recv_message() {
+            Ok(msg) => match msg {
+                websocket::OwnedMessage::Text(text) => {
+                    let op: Result<i32, _> = text.parse();
+                    match op {
+                        Ok(op) => {
+                            // let cmd = CommandType::from(op);
+                        }
+                        Err(e) => {
+                            eprintln!("Encountered error when parsing operation request: {e}");
+                        }
+                    }
+                    // println!("Received from client: {text}");
+                }
+                websocket::OwnedMessage::Binary(_items) => todo!(),
+                websocket::OwnedMessage::Close(close_data) => {
+                    match close_data {
+                        Some(close_data) => {
+                            println!("Client closed with: {close_data:?}");
+                        }
+                        None => {}
+                    }
+                    return;
+                }
+                websocket::OwnedMessage::Ping(_items) => todo!(),
+                websocket::OwnedMessage::Pong(_items) => todo!(),
+            },
+
+            Err(websocket::WebSocketError::NoDataAvailable) => {
+                println!("Client disconnected");
+                return;
+            }
+
+            Err(e) => {
+                println!("Received invalid message: {e:?}");
+                return;
             }
         }
     }
