2c0f76aa583dc55fb2a192dbd18b0da6f1e6319d
steve0-t <stefan.simai@mailbox.org>
2026-07-13 19:10:47 +0200

    add cancellation token

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 use anyhow::{Context, Result};
 
+use tokio::signal;
 use tokio_rustls::rustls::{
     ClientConfig, RootCertStore,
     client::AlwaysResolvesClientRawPublicKeys,
@@ -15,6 +16,7 @@ use tokio_rustls::rustls::{
 
 use futures_util::{self, SinkExt, StreamExt, TryStreamExt};
 use tokio_tungstenite::tungstenite::{Message, Utf8Bytes};
+use tokio_util::sync::CancellationToken;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -56,10 +58,20 @@ async fn main() -> Result<()> {
 
     let (mut write, mut read) = ws_stream.split();
 
+    // Step 1: Create a new CancellationToken
+    let token = CancellationToken::new();
+
+    // Step 2: Clone the token for use in another task
+    let cloned_token = token.clone();
+
     let read_handle = tokio::spawn(async move {
         while let Some(msg) = read.next().await {
             match msg {
                 Ok(msg) => {
+                    if msg.is_close() {
+                        token.cancel();
+                        return;
+                    }
                     println!("Client: Received message: {msg}");
                 }
                 Err(e) => {
@@ -72,16 +84,25 @@ async fn main() -> Result<()> {
     let mut reader = io::BufReader::new(io::stdin()).lines();
     let write_handle = tokio::spawn(async move {
         while let Some(line) = reader.next() {
+            if cloned_token.is_cancelled() {
+                return;
+            }
+
             match line {
                 Ok(msg) => match write.send(Message::text(msg)).await {
                     Ok(_) => {
                         println!("Client: successfully sent a message");
                     }
                     Err(e) => {
+                        if e.to_string().contains("Broken pipe") {
+                            break;
+                        }
                         eprintln!("Client: failed to send a message: {e:?}");
                     }
                 },
-                Err(_) => todo!(),
+                Err(e) => {
+                    eprintln!("Client: failed to read input: {e:?}");
+                }
             }
         }
     });
