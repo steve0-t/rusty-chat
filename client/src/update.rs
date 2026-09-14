e14ea7f5636ef9ed85fbc9d30e8c0c1de66411dd
steve0-t <stefan.simai@mailbox.org>
2026-09-14 22:26:53 +0200

    just testing

--- client/src/update.rs
+++ client/src/update.rs
@@ -5,8 +5,13 @@ use crate::app::App;
 pub fn update(app: &mut App, key_event: KeyEvent) {
     match key_event.code {
         KeyCode::Esc | KeyCode::Char('q') => app.quit(),
+
         KeyCode::Char('c') | KeyCode::Char('C') if key_event.modifiers == KeyModifiers::CONTROL => {
             app.quit()
+        }
+
+        KeyCode::Char(to_process) => {
+            app.in_buf.push(to_process);
         }
 
         _ => {}
